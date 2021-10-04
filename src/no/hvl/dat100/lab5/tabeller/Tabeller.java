c2588ef3031ba0f8b190448de673db23ca8cc72d
Kristianalek <89082168+Kristianalek@users.noreply.github.com>
2021-10-04 21:42:50 +0200

    Update Tabeller.java

--- src/no/hvl/dat100/lab5/tabeller/Tabeller.java
+++ src/no/hvl/dat100/lab5/tabeller/Tabeller.java
@@ -1,28 +1,48 @@
 package no.hvl.dat100.lab5.tabeller;
-
 public class Tabeller {
-
+	
+	public static void main(String[] args) {
+			int[] tabell = {1, 2, 5, 7, 8};
+			int[] tabellb = {42, 67, 89};
+		skrivUt(tabell);
+		tilStreng(tabellb);
+		int summer = summer(tabellb); 
+		
+		System.out.println(summer);
+		 
+		
+	}
+	
 	// a)
 	public static void skrivUt(int[] tabell) {
-
-		// TODO
-		throw new UnsupportedOperationException("skrivUt ikke implementert");
-
+	
+		for (int i = 0; i<tabell.length; i++) {
+			System.out.print(tabell[i]);
+		}
 	}
 
 	// b)
 	public static String tilStreng(int[] tabell) {
-
-		// TODO
-		throw new UnsupportedOperationException("tilStreng ikke implementert");
 		
+		String en = "["; 
+		
+		for (int i = 0; i<tabell.length-1; i++) {
+			String kopi = Integer.toString(tabell[i]);
+			en = en + kopi + ",";
+		}
+		System.out.println(en + tabell[tabell.length-1] + "]");
+		return null;
 	}
+		
 
 	// c)
 	public static int summer(int[] tabell) {
-
-		// TODO
-		throw new UnsupportedOperationException("summer ikke implementert");
+		int sum = 0;
+		for (int i = 0; i<tabell.length; i++) {
+			sum = sum + tabell[i];
+		}	
+			return sum;
+			
 	}
 
 	// d)
